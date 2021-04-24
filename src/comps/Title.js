31894d537298e3096ed4dd8a1d7600cb84cecfec
arvindIncrescotech <82077817+arvindIncrescotech@users.noreply.github.com>
2021-04-25 00:04:12 +0530

    header formated

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -1,13 +1,24 @@
 import React from 'react';
-
+import welcome from '../assests/welcome.svg';
 const Title = () => {
   return (
     <div className="title">
-      {/* <h1>FireGram</h1> */}
-      {/* <h2>Your Pictures</h2> */}
-      {/* <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> */}
+      <h1>Welcome Dear</h1>
+      <div className="header">
+        <div>
+          <img
+            src={welcome}
+            alt="welcome"
+            style={{ height: 200, width: 500 }}
+          />
+        </div>
+        <div>
+          <h2>Photo Gallery</h2>
+          <p> Great you came here !! Start uploading your picture down</p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Title;
