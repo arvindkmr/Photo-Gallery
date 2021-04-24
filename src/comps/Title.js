79b9640f5f922491a85fc3f7ac8f549ae607d668
arvindIncrescotech <82077817+arvindIncrescotech@users.noreply.github.com>
2021-04-25 00:14:52 +0530

    design ready

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -2,19 +2,23 @@ import React from 'react';
 import welcome from '../assests/welcome.svg';
 const Title = () => {
   return (
-    <div className="title">
-      <h1>Welcome Dear</h1>
-      <div className="header">
-        <div>
-          <img
-            src={welcome}
-            alt="welcome"
-            style={{ height: 200, width: 500 }}
-          />
-        </div>
-        <div>
-          <h2>Photo Gallery</h2>
-          <p> Great you came here !! Start uploading your picture down</p>
+    <div>
+      <div className="topBar">
+        <h1>Welcome Dear</h1>
+      </div>
+      <div className="title">
+        <div className="header">
+          <div>
+            <img
+              src={welcome}
+              alt="welcome"
+              style={{ height: 200, width: 500 }}
+            />
+          </div>
+          <div>
+            <h2>Photo Gallery</h2>
+            <p> Great you came here !! Start uploading your picture down</p>
+          </div>
         </div>
       </div>
     </div>
